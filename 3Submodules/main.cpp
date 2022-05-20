5edd7d8c83c8ed181d7d2e96d171fc4bb524e023
minisparrow <melchior_faith@163.com>
2022-05-20 16:28:07 +0800

    add submodules

---
+++ 3Submodules/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream> 
+#include <stdio.h> 
+#include <Adder.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    using namespace std;
+    cout << "hello cmake." << endl;
+    cout << adder(1.2, 2.3) << endl;
+
+    //glfw demo
+    GLFWwindow* window;
+    int width, height;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+    }
+
+    glfwWindowHint(GLFW_DEPTH_BITS, 16);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+    }
+
+    // Set callback functions
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval( 1 );
+
+    glfwGetFramebufferSize(window, &width, &height);
+
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    return 0;
+}
